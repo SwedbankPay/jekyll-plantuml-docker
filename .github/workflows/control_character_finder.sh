c4041fa77eb11aacd5001f227aaf638206753bac
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-05-19 14:38:16 +0200

    Move control character detection to script file

---
+++ .github/workflows/control_character_finder.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+files_with_control_characters=$(\
+    grep --color='always' \
+        --perl-regexp \
+        --line-number \
+        --byte-offset \
+        --exclude-dir='.git' \
+        --recursive \
+        "[\x80-\xFF]" \
+        . \
+)
+if [[ ! -z $files_with_control_characters ]]; then
+  echo "Files with control characters detected!"
+  echo ""
+  echo "$files_with_control_characters"
+  exit 1
+else
+  echo "No control characters detected. All good."
+fi
