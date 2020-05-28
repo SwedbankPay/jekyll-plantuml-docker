e2f9d1cd6759a6a02bb3cc8a11bc0665bd56e931
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-05-28 22:44:44 +0200

    Split docker_run_test into more functions

--- .github/workflows/docker_run_test.sh
+++ .github/workflows/docker_run_test.sh
@@ -4,27 +4,34 @@ initialize() {
 	docker_tag=${TAG:-latest}
 	local_directory=${JEKYLL_DIR:-"$PWD"}
 	search_string=${OUTPUT:-"Server running"}
+    jekyll_command=${COMMAND}
 }
 
 docker_run() {
+    # shellcheck disable=SC2086
+    docker run \
+        --tty \
+        --volume "${local_directory}:/srv/jekyll" \
+        "swedbankpay/jekyll-plantuml:${docker_tag}" \
+        $jekyll_command \
+        & echo $! > pid;
+}
+
+docker_test() {
+    grep -m1 "${search_string}" \
+        && kill -9 "$(cat pid)" \
+        && rm pid;
+}
+
+docker_run_and_test() {
     echo "Running swedbankpay/jekyll-plantuml:${docker_tag}..."
 
-    { \
-        docker run \
-            --tty \
-            --volume "${local_directory}:/srv/jekyll" \
-            "swedbankpay/jekyll-plantuml:${docker_tag}" \
-            & echo $! > pid; \
-    } | tee /dev/stderr | { \
-        grep -m1 "${search_string}" \
-        && kill -9 "$(cat pid)" \
-        && rm pid; \
-    }
+    docker_run | tee /dev/stderr | docker_test
 }
 
 main() {
 	initialize
-	docker_run
+	docker_run_and_test
 }
 
 main
