47dc401b884974a69b94489a18fa158d2b600230
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-05-30 01:50:58 +0200

    Readjust Docker labels
    - Replace label keys with ones from label-schema.org
    - Add a lot of new Docker labels
    - Provide version number with GitVersion

--- .github/workflows/variables.sh
+++ .github/workflows/variables.sh
@@ -12,6 +12,7 @@ Generates variables based on the provided <sha> and <ref>.
 initialize() {
 	sha="$1"
     ref="$2"
+    version="$3"
 
     if [[ -z "$sha" ]]; then
         echo "No sha specified." >&2
@@ -24,24 +25,33 @@ initialize() {
         echo "$help_message"
         exit 1
     fi
+
+    if [[ -z "$version" ]]; then
+        echo "No version specified." >&2
+        echo "$help_message"
+        exit 1
+    fi
 }
 
 generate_variables() {
     sha8=$(echo "${sha}" | cut -c1-8)
-    date=$(date +%F)
+    date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
 
+    # Override GitVersion's version on tags, just to be sure.
     if [[ "$ref" == refs/tags/* ]]; then
         version="${ref#refs/tags/}"
-    else
-        version="$sha8"
     fi
 
+    echo "Ref:     ${ref}"
+    echo "Sha:     ${sha}"
+    echo "Sha8:    ${sha8}"
+    echo "Date:    ${date}"
     echo "Version: ${version}"
-    echo "SHA8: ${sha8}"
-    echo "Date: ${date}"
-    echo "::set-output name=version::${version}"
+    echo "::set-output name=ref::${ref}"
+    echo "::set-output name=sha::${sha}"
     echo "::set-output name=sha8::${sha8}"
     echo "::set-output name=date::${date}"
+    echo "::set-output name=version::${version}"
 }
 
 main() {
