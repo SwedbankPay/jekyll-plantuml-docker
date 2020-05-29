4097955f0cef5a6ab797597b427c7fd0f622b59e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-05-29 23:42:39 +0200

    Use variables.sh to provide tag, date and sha8 everywhere

--- .github/workflows/variables.sh
+++ .github/workflows/variables.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
+set -o errexit #abort if any command fails
+me=$(basename "$0")
 
-if [[ -z "$SHA" ]]; then
-    echo "Environment variable SHA missing."
-    exit 1
-fi
+help_message="\
+Usage: $me <sha> <ref>
+Generates variables based on the provided <sha> and <ref>.
 
-if [[ -z "$REF" ]]; then
-    echo "Environment variable REF missing."
-    exit 1
-fi
+<sha>: The SHA of the curretn Git commit.
+<ref>: The name of the ref of the current Git commit."
 
-if [[ "$REF" == refs/tags/* ]]; then
-    version="${REF#refs/tags/}"
-else
-    version="$(echo "${SHA}" | cut -c1-8)"
-fi
+initialize() {
+	sha="$1"
+    ref="$2"
 
-echo "Version: ${version}"
-echo "::set-output name=version::${version}"
+    if [[ -z "$sha" ]]; then
+        echo "No sha specified." >&2
+        echo "$help_message"
+        exit 1
+    fi
+
+    if [[ -z "$ref" ]]; then
+        echo "No ref specified." >&2
+        echo "$help_message"
+        exit 1
+    fi
+}
+
+generate_variables() {
+    sha8=$(echo "${sha}" | cut -c1-8)
+    date=$(date +%F)
+
+    if [[ "$ref" == refs/tags/* ]]; then
+        version="${ref#refs/tags/}"
+    else
+        version="$sha8"
+    fi
+
+    echo "Version: ${version}"
+    echo "SHA8: ${sha8}"
+    echo "Date: ${date}"
+    echo "::set-output name=version::${version}"
+    echo "::set-output name=sha8::${sha8}"
+    echo "::set-output name=date::${date}"
+}
+
+main() {
+	initialize "$@"
+    generate_variables
+}
+
+main "$@"
