3e0f7a5d9fadd930c0c59fe203aa9ec7a52398cb
NullableInt <daniel.granerud@payex.com>
2020-06-04 15:24:47 +0200

    DX-1004 Add a shellcheck ignore

--- .github/workflows/verify_build_output.sh
+++ .github/workflows/verify_build_output.sh
@@ -4,6 +4,7 @@ set -o errexit #abort if any command fails
 [ "${DEBUG:-false}" = "true" ] && set -x
 
 #Spin up docker
+# shellcheck disable=SC2046
 docker run --tty --volume $(pwd)/.docker/jekyll-plantuml/:/srv/jekyll swedbankpay/jekyll-plantuml:latest -d
 
 bundle install --gemfile ./.docker/rake/Gemfile
