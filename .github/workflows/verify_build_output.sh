71201adac8173969b72d7d3f5dcf7456503d4581
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-05 20:16:00 +0200

    DX-1004: Use GPR image name
    Use docker.pkg.github.com/swedbankpay/jekyll-plantuml-docker/jekyll-plantuml as image name

--- .github/workflows/verify_build_output.sh
+++ .github/workflows/verify_build_output.sh
@@ -8,9 +8,10 @@ if [[ -z "$IMAGE_TAG" ]]; then
     exit 1
 fi
 
-image_fqn="swedbankpay/jekyll-plantuml:$IMAGE_TAG"
+docker_image_name="${IMAGE_NAME:-swedbankpay/jekyll-plantuml}"
+docker_image_fqn="$docker_image_name:$IMAGE_TAG"
 
-echo "Running $image_fqn"
+echo "Running $docker_image_fqn"
 
 # Spin up docker
 docker run \
@@ -18,7 +19,7 @@ docker run \
     --publish 4000:4000 \
     --tty \
     --volume "$(pwd)/.docker/jekyll-plantuml:/srv/jekyll" \
-    "$image_fqn"
+    "$docker_image_fqn"
 
 gem install bundler
 bundle install --gemfile ./.docker/rake/Gemfile
@@ -27,7 +28,7 @@ sudo apt-get install libcurl4
 
 rake -f ./.docker/rake/Rakefile
 
-container_id=$(docker ps -a -q --filter ancestor="$image_fqn" --format="{{.ID}}")
+container_id=$(docker ps -a -q --filter ancestor="$docker_image_fqn" --format="{{.ID}}")
 
 echo "Stopping $container_id"
 docker stop "$container_id"
