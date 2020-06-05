fda16cd706273b6ac979590e3a3ea930fea60435
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-05 14:47:08 +0200

    DX-1004: Add IMAGE_TAG to the verify build output script

--- .github/workflows/verify_build_output.sh
+++ .github/workflows/verify_build_output.sh
@@ -3,9 +3,14 @@ set -o errexit #abort if any command fails
 
 [ "${DEBUG:-false}" = "true" ] && set -x
 
+if [[ -z "$IMAGE_TAG" ]]; then
+    echo "Missing IMAGE_TAG environment variable. Aborting."
+    exit 1
+fi
+
 #Spin up docker
 # shellcheck disable=SC2046
-docker run -d -p 4000:4000 --tty --volume $(pwd)/:/srv/jekyll swedbankpay/jekyll-plantuml:latest
+docker run -d -p 4000:4000 --tty --volume $(pwd)/:/srv/jekyll "swedbankpay/jekyll-plantuml:$IMAGE_TAG"
 
 gem install bundler
 bundle install --gemfile ./.docker/rake/Gemfile
@@ -14,4 +19,4 @@ sudo apt-get install libcurl4
 
 rake -f ./.docker/./rake/Rakefile
 
-docker stop "$(docker ps -a -q --filter ancestor=swedbankpay/jekyll-plantuml:latest --format="{{.ID}}")"
+docker stop "$(docker ps -a -q --filter ancestor="swedbankpay/jekyll-plantuml:$IMAGE_TAG" --format="{{.ID}}")"
