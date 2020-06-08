fee40357bdd2b550f20fdb99abc2a35411e5eb70
NullableInt <daniel.granerud@payex.com>
2020-06-08 10:21:03 +0200

    DX-1004 Move docker to Rakefile for verifying output

--- .github/workflows/verify_build_output.sh
+++ .github/workflows/verify_build_output.sh
@@ -3,26 +3,7 @@ set -o errexit #abort if any command fails
 
 [ "${DEBUG:-false}" = "true" ] && set -x
 
-if [[ -z "$IMAGE_TAG" ]]; then
-    echo "Missing IMAGE_TAG environment variable. Aborting."
-    exit 1
-fi
-
-docker_image_name="${IMAGE_NAME:-swedbankpay/jekyll-plantuml}"
-docker_image_fqn="$docker_image_name:$IMAGE_TAG"
-
 cd test
-
-echo "Running $docker_image_fqn"
-
-# Spin up docker
-docker run \
-    --detach \
-    --publish 4000:4000 \
-    --tty \
-    --volume "$(pwd):/srv/jekyll" \
-    "$docker_image_fqn" \
-    jekyll build
 
 echo "Installing gems"
 
