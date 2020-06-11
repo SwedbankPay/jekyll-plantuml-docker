235cd563246754eb6c0c0b539e39a00aefe25f93
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-11 03:04:22 +0200

    DX-1004: Improve config generation
    Improve config generation with a method and add support for build and serve

--- .docker/jekyll-plantuml/entrypoint.rb
+++ .docker/jekyll-plantuml/entrypoint.rb
@@ -3,49 +3,64 @@ require "jekyll"
 JEKYLL_VAR_DIR = ENV.fetch("JEKYLL_VAR_DIR")
 JEKYLL_ENV = ENV.fetch("JEKYLL_ENV", "production")
 
+def get_config(command)
+  config_file_path = File.join(Dir.pwd, "_config.yml")
+
+  jekyll_config = {
+    "config" => config_file_path,
+    "incremental" => true,
+  }
+
+  unless File.file?(config_file_path)
+    default_config_file_path = File.join(JEKYLL_VAR_DIR, "_config.default.yml")
+    puts "No _config.yml found. Using default: #{default_config_file_path}"
+    jekyll_config["config"] = default_config_file_path
+  end
+
+  if command == "serve"
+    jekyll_config.merge({
+      "host" => "0.0.0.0",
+      "port" => "4000",
+      "livereload" => true,
+      "force_polling" => true,
+      "watch" => true,
+    })
+  end
+
+  jekyll_config
+end
+
 unless File.file?(File.join(Dir.pwd, "Gemfile"))
   default_gemfile_path = File.join(JEKYLL_VAR_DIR, "Gemfile")
   puts "No Gemfile found. Using default: #{default_gemfile_path}"
   ENV["BUNDLE_GEMFILE"] = default_gemfile_path
 end
 
-config_file_path = File.join(Dir.pwd, "_config.yml")
-
-jekyll_config = {
-  "host" => "0.0.0.0",
-  "port" => "4000",
-  "livereload" => true,
-  "incremental" => true,
-  "force_polling" => true,
-  "watch" => true,
-  "config" => config_file_path,
-}
-
-unless File.file?(File.join(Dir.pwd, "_config.yml"))
-  default_config_file_path = File.join(JEKYLL_VAR_DIR, "_config.default.yml")
-  puts "No _config.yml found. Using default: #{default_config_file_path}"
-  jekyll_config["config"] = default_config_file_path
-end
-
 if ARGV.length == 0
   puts "Running default command 'jekyll serve' (JEKYLL_ENV: #{JEKYLL_ENV})..."
+  jekyll_config = get_config("serve")
   Jekyll::Commands::Serve.process(jekyll_config)
 elsif ARGV[0] == "deploy"
   puts "Deploying..."
+  jekyll_config = get_config("deploy")
   Jekyll::Commands::Build.process(jekyll_config)
   exec("/usr/jekyll/bin/deploy.sh --verbose")
 elsif ARGV[0] == "jekyll"
   requested_jekyll_command = ARGV[1]
-  all_jekyll_commands = Jekyll::Commands.constants.select { |c| Jekyll::Commands.const_get(c).is_a? Class }
-  matching_jekyll_command = all_jekyll_commands.find { |c| c.to_s.casecmp(requested_jekyll_command) == 0 }
 
-  unless matching_jekyll_command
-    raise "Jekyll command '#{requested_jekyll_command}' not understood."
+  if requested_jekyll_command.empty?
+    raise "No jekyll command provided."
   end
 
-  puts "Running Jekyll command '#{requested_jekyll_command}' (JEKYLL_ENV: #{JEKYLL_ENV})..."
+  requested_jekyll_command = requested_jekyll_command.downcase
+  jekyll_config = get_config(requested_jekyll_command)
 
-  Jekyll::Commands.const_get(matching_jekyll_command).process(jekyll_config)
+  case requested_jekyll_command.downcase
+  when "build"
+    Jekyll::Commands::Build.process(jekyll_config)
+  when "serve"
+    Jekyll::Commands::Serve.process(jekyll_config)
+  end
 
   # If the default_config_file is assigned and the jekyll command supports '--config',
   # apply the default config by performing some positional argument magic.
