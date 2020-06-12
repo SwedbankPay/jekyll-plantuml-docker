6084a8355cb267a5553a172f20ce6df80f0433a8
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-12 12:59:16 +0200

    DX-1004: Run docker in .sh before invoking Rake.

--- .github/workflows/verify_build_output.sh
+++ .github/workflows/verify_build_output.sh
@@ -18,4 +18,17 @@ echo "Configuring and installing gems..."
 bundle config path "$(pwd)/vendor/bundle"
 bundle check || bundle install
 
+docker_image_name="${IMAGE_NAME:-swedbankpay/jekyll-plantuml}"
+docker_image_tag="${IMAGE_TAG:-latest}"
+docker_image_fqn="${docker_image_name}:${docker_image_tag}"
+
+echo "Running $docker_image_fqn..."
+
+docker run \
+    --env JEKYLL_ENV=production \
+    --tty \
+    --volume "$(pwd):/srv/jekyll" \
+    "$docker_image_fqn" \
+    jekyll build
+
 bundle exec rake
