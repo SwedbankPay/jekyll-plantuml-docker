49771b879d1816181c69afd360df255a711cf508
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-18 23:22:53 +0200

    DX-1004: Fix SC2086: Double quote to prevent globbing and word splitting

--- .docker/entrypoint/sh/entrypoint.sh
+++ .docker/entrypoint/sh/entrypoint.sh
@@ -9,6 +9,6 @@ default_gemfile="${JEKYLL_VAR_DIR}/entrypoint/Gemfile_generated"
 bundle check --gemfile="$default_gemfile" \
   || bundle install --gemfile="$default_gemfile"
 
-bundle config set gemfile $default_gemfile
+bundle config set gemfile "$default_gemfile"
 
 exec bundle exec ruby "${JEKYLL_VAR_DIR}/entrypoint/lib/entrypoint.rb" "$@"
