251b53b5e7138dabac3f4b1fef43e5baf6743e8f
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-19 22:12:08 +0200

    DX-1004: Add some debug statements to GemfileGenerator

--- .docker/entrypoint/lib/gemfile-generator.rb
+++ .docker/entrypoint/lib/gemfile-generator.rb
@@ -16,13 +16,17 @@ module Jekyll
           raise "#{secondary_gemfile_path} cannot be found."
         end
 
+        puts "Reading #{primary_gemfile_path}..." if @debug
         original_file = File.readlines File.join(primary_gemfile_path)
         generated_file = original_file
 
+        puts "Diffing #{secondary_gemfile_path}..." if @debug
         gemfile_differ = Jekyll::PlantUml::GemfileDiffer.new(@debug)
         gemfile_differ.diff(primary_gemfile_path, secondary_gemfile_path) do |line|
           generated_file << line
         end
+
+        puts "Generating #{generated_gemfile_path}..." if @debug
         File.open(generated_gemfile_path, "w") { |file| file.puts(generated_file) }
       end
     end
