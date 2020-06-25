e197ea513300e69c5098a0033a8ff555521b828e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-26 01:26:06 +0200

    DX-1063: Add DockerImage spec

---
+++ .docker/entrypoint/spec/docker_image_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'docker_image'
+
+describe Jekyll::PlantUml::DockerImage do
+  subject(:docker_image) { Jekyll::PlantUml::DockerImage.new('jekyll-plantuml', 'latest', '1.2.3') }
+  subject { docker_image }
+
+  describe '#initialize' do
+    it {
+      is_expected.not_to be_nil
+    }
+    it {
+      is_expected.to have_attributes(
+        name: 'jekyll-plantuml',
+        tag: 'latest',
+        version: '1.2.3',
+        fqn: 'jekyll-plantuml:latest'
+      )
+    }
+
+    context 'name is nil' do
+      it do
+        expect do
+          Jekyll::PlantUml::DockerImage.new(nil, 'latest', '1.2.3')
+        end.to raise_error(ArgumentError, 'name is nil')
+      end
+    end
+
+    context 'tag is nil' do
+      it do
+        expect do
+          Jekyll::PlantUml::DockerImage.new('jekyll-plantuml', nil, '1.2.3')
+        end.to raise_error(ArgumentError, 'tag is nil')
+      end
+    end
+
+    context 'version is nil' do
+      it do
+        expect do
+          Jekyll::PlantUml::DockerImage.new('jekyll-plantuml', 'latest', nil)
+        end.to raise_error(ArgumentError, 'version is nil')
+      end
+    end
+  end
+
+  describe '#to_s' do
+    subject { docker_image.to_s }
+    it {
+      expect(subject.to_s).to eq('jekyll-plantuml:latest')
+    }
+  end
+end
