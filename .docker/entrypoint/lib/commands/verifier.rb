4f9eae33bf303f8435c675806fe9ac2815618f15
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-06-30 01:19:07 +0200

    DX-1071: Add Verifier spec with ignored URLs

--- .docker/entrypoint/lib/commands/verifier.rb
+++ .docker/entrypoint/lib/commands/verifier.rb
@@ -3,6 +3,7 @@
 require 'jekyll'
 require 'html-proofer'
 require 'html-proofer-unrendered-markdown'
+require_relative '../file_not_found_error'
 
 # The Jekyll module contains everything related to Jekyll.
 module Jekyll
@@ -15,19 +16,31 @@ module Jekyll
       # Jekyll site in order to verify that the HTML is correct.
       class Verifier
         def initialize(jekyll_config)
-          raise 'Nil or empty Jekyll config' if jekyll_config.nil? || jekyll_config.empty?
-          raise "No 'destination' key found in the Jekyll config" unless jekyll_config.key? 'destination'
+          raise ArgumentError, 'jekyll_config cannot be nil' if jekyll_config.nil?
+          raise ArgumentError, 'jekyll_config must be a hash' unless jekyll_config.is_a? Hash
+          raise ArgumentError, 'jekyll_config cannot be empty' if jekyll_config.empty?
+          raise ArgumentError, "No 'destination' key found in the Jekyll config" unless jekyll_config.key? 'destination'
 
           @jekyll_destination_dir = jekyll_config['destination']
-          raise "#{@jekyll_destination_dir} does not exist" unless Dir.exist?(@jekyll_destination_dir)
+          unless Dir.exist?(@jekyll_destination_dir)
+            raise Jekyll::PlantUml::FileNotFoundError, "#{@jekyll_destination_dir} does not exist"
+          end
         end
 
-        def verify
+        def verify(ignore_urls = nil)
           html_glob = File.join(@jekyll_destination_dir, '**/*.html')
 
           raise "#{@jekyll_destination_dir} contains no .html files" if Dir.glob(html_glob).empty?
 
-          options = {
+          opts = options(ignore_urls)
+
+          HTMLProofer.check_directory(@jekyll_destination_dir, opts).run
+        end
+
+        private
+
+        def options(ignore_urls = nil)
+          opts = {
             assume_extension: true,
             check_html: true,
             enforce_https: true,
@@ -35,7 +48,9 @@ module Jekyll
             check_unrendered_link: true
           }
 
-          HTMLProofer.check_directory(@jekyll_destination_dir, options).run
+          return opts if ignore_urls.nil? || !ignore_urls.is_a?(Array) || ignore_urls.empty?
+
+          opts.merge({ url_ignore: ignore_urls })
         end
       end
     end
