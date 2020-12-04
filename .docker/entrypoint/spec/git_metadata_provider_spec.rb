0b6b61e089b9b74000705bcd49edcebd69f658f1
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-12-04 22:27:06 +0100

    DX-1446: Add spec for translation of Git SSH URLs

--- .docker/entrypoint/spec/git_metadata_provider_spec.rb
+++ .docker/entrypoint/spec/git_metadata_provider_spec.rb
@@ -5,16 +5,34 @@ require_relative 'includes'
 
 describe GitMetadataProvider do
   describe '#initialize' do
-    data_dir = File.join(__dir__, 'data')
-    branch = 'my_favorite_branch'
-    repo = 'https://example.com/SwedbankPay/nice_repository'
+    context 'values extracted from context' do
+      data_dir = File.join(__dir__, 'data')
+      branch = 'my_favorite_branch'
+      repo = 'https://example.com/SwedbankPay/nice_repository'
 
-    subject do
-      context = Context.new('development', __dir__, data_dir, git_branch: branch, git_repository_url: repo)
-      GitMetadataProvider.new(context)
+      subject do
+        context = Context.new('development', __dir__, data_dir, git_branch: branch, git_repository_url: repo)
+        GitMetadataProvider.new(context)
+      end
+
+      its(:branch) { is_expected.to eq branch }
+      its(:repository_url) { is_expected.to eq repo }
     end
 
-    its(:branch) { is_expected.to eq branch }
-    its(:repository_url) { is_expected.to eq repo }
+    context 'git URL is translated' do
+      data_dir = File.join(__dir__, 'data')
+
+      subject do
+        context = Context.new(
+          'development',
+          __dir__,
+          data_dir,
+          git_branch: 'master',
+          git_repository_url: 'git@github.com:SwedbankPay/nice_repository.git')
+        GitMetadataProvider.new(context)
+      end
+
+      its(:repository_url) { is_expected.to eq 'https://github.com/SwedbankPay/nice_repository' }
+    end
   end
 end
