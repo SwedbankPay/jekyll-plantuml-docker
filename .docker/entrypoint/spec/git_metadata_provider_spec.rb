50f4231f312ef1df2989f441390cb186294ace47
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-12-04 22:30:21 +0100

    DX-1446: Add spec for values extracted from Git

--- .docker/entrypoint/spec/git_metadata_provider_spec.rb
+++ .docker/entrypoint/spec/git_metadata_provider_spec.rb
@@ -34,5 +34,17 @@ describe GitMetadataProvider do
 
       its(:repository_url) { is_expected.to eq 'https://github.com/SwedbankPay/nice_repository' }
     end
+
+    context 'values extracted from Git' do
+      data_dir = File.join(__dir__, 'data')
+
+      subject do
+        context = Context.new('development', __dir__, data_dir)
+        GitMetadataProvider.new(context)
+      end
+
+      its(:branch) { is_expected.not_to be_empty }
+      its(:repository_url) { is_expected.to eq 'https://github.com/SwedbankPay/jekyll-plantuml-docker' }
+    end
   end
 end
