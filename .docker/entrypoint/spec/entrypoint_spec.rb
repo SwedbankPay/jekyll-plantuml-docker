1cc46d9d97b57fb03667a3156482584a54b332f6
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-12-08 23:34:06 +0100

    DX-1446: Add spec for parameterless Entrypoint#initialize

--- .docker/entrypoint/spec/entrypoint_spec.rb
+++ .docker/entrypoint/spec/entrypoint_spec.rb
@@ -3,19 +3,46 @@
 require 'includes'
 
 describe Entrypoint do
-  subject(:entrypoint) do
+  describe '#initialize' do
     data_dir = File.join(__dir__, 'data')
-    Entrypoint.new(
-      Context.new('dev', data_dir, data_dir),
-      DockerImage.new('jekyll-plantuml', 'latest', '1.2.3')
-    )
-  end
 
-  it {
-    is_expected.not_to be_nil
-  }
+    context 'with arguments' do
+      subject do
+        Entrypoint.new(
+          Context.new('dev', data_dir, data_dir),
+          DockerImage.new('jekyll-plantuml', 'latest', '1.2.3')
+        )
+      end
 
-  describe '#execute' do
-    it { expect { entrypoint.execute }.to_not raise_error }
+      it {
+        is_expected.not_to be_nil
+      }
+
+      describe '#execute' do
+        it { expect { subject.execute }.to_not raise_error }
+      end
+    end
+
+    context 'from environment variables' do
+      include_context 'env_helper'
+
+      before(:all) {
+        env({
+          'JEKYLL_VAR_DIR' => data_dir,
+          'JEKYLL_DATA_DIR' => data_dir,
+          'GITHUB_BRANCH' => 'branch',
+          'GITHUB_REPOSITORY_URL' => 'repo',
+          'DOCKER_IMAGE_NAME' => 'img',
+          'DOCKER_IMAGE_TAG' => 'tag',
+          'DOCKER_IMAGE_VERSION' => '1.0'
+        })
+      }
+
+      subject { Entrypoint.new }
+
+      it {
+        is_expected.not_to be_nil
+      }
+    end
   end
 end
