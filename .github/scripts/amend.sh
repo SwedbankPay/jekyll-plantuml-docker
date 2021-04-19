3f41f8f34aafc49f2eb9c007143288870f72e2c3
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-04-20 01:45:45 +0200

    Explicitly authenticate against the remote repo

--- .github/scripts/amend.sh
+++ .github/scripts/amend.sh
@@ -3,18 +3,22 @@ set -o errexit # Abort if any command fails
 me=$(basename "$0")
 
 help_message="\
-Usage: echo $me
+Usage: echo $me --user <username>
 
 Checks out the pull request where an '/amend' comment is made and amends its
 latest commit with the credentials of the user who wrote the '/amend' comment.
 
 Arguments:
+  -u, --user      The user name to use when authenticating against the remote
+                  Git repository.
   -h, --help      Displays this help screen.
   -v, --verbose   Increase verbosity. Useful for debugging.
 
 Environment Variables:
   GITHUB_CONTEXT: An environment variable containing a JSON string of the GitHub
-                  context object. Typically generated with \${{ toJson(github) }}."
+                  context object. Typically generated with \${{ toJson(github) }}.
+  GITHUB_TOKEN:   The personal access token to use to authenticate against the
+                  remote Git repository."
 
 parse_args() {
     while : ; do
@@ -24,6 +28,9 @@ parse_args() {
         elif [[ $1 = "-v" || $1 = "--verbose" ]]; then
             verbose=true
             shift
+        elif [[ ( $1 = "-u" || $1 = "--user" ) && -n $2 ]]; then
+            auth_user=$2
+            shift 2
         else
             break
         fi
@@ -39,10 +46,23 @@ parse_github_context() {
         exit 1
     fi
 
+    if [[ -z "$GITHUB_TOKEN" ]]; then
+        echo "Missing or empty GITHUB_TOKEN environment variable." >&2
+        echo "$help_message"
+        exit 1
+    fi
+
+    if [[ -z "$auth_user" ]]; then
+        echo "Missing or empty 'auth_user' argument." >&2
+        echo "$help_message"
+        exit 1
+    fi
+
     pr_url=$(echo "$github_context_json" | jq --raw-output .event.issue.pull_request.html_url)
-    username=$(echo "$github_context_json" | jq --raw-output .event.sender.login)
-    user_url=$(echo "$github_context_json" | jq --raw-output .event.sender.url)
+    sender_login=$(echo "$github_context_json" | jq --raw-output .event.sender.login)
+    sender_url=$(echo "$github_context_json" | jq --raw-output .event.sender.url)
     repo_url=$(echo "$github_context_json" | jq --raw-output .event.repository.html_url)
+    repo_clone_url=$(echo "$github_context_json" | jq --raw-output .event.repository.clone_url)
     collaborators_url=$(echo "$github_context_json" | jq --raw-output .event.repository.collaborators_url)
 
     if [[ -z "$pr_url" ]]; then
@@ -51,13 +71,13 @@ parse_github_context() {
         exit 1
     fi
 
-    if [[ -z "$username" ]]; then
+    if [[ -z "$sender_login" ]]; then
         echo "No 'event.sender.login' found in the GitHub context." >&2
         echo "$help_message"
         exit 1
     fi
 
-    if [[ -z "$user_url" ]]; then
+    if [[ -z "$sender_url" ]]; then
         echo "No 'event.sender.url' found in the GitHub context." >&2
         echo "$help_message"
         exit 1
@@ -69,16 +89,22 @@ parse_github_context() {
         exit 1
     fi
 
+    if [[ -z "$repo_clone_url" ]]; then
+        echo "No 'event.repository.clone_url' found in the GitHub context." >&2
+        echo "$help_message"
+        exit 1
+    fi
+
     if [[ -z "$collaborators_url" ]]; then
         echo "No 'event.repository.collaborators_url' found in the GitHub context." >&2
         echo "$help_message"
         exit 1
     fi
 
-    user_json=$(gh api -X GET "$user_url")
+    user_json=$(gh api -X GET "$sender_url")
 
     if [[ -z "$user_json" ]]; then
-        echo "The request for '$user_url' failed." >&2
+        echo "The request for '$sender_url' failed." >&2
         echo "$help_message"
         exit 1
     fi
@@ -87,20 +113,23 @@ parse_github_context() {
     email=$(echo "$user_json" | jq --raw-output .email)
 
     if [[ -z "$name" ]]; then
-        echo "No 'name' found in '$user_url'." >&2
+        echo "No 'name' found in '$sender_url'." >&2
         echo "$help_message"
         exit 1
     fi
 
     if [[ -z "$email" ]]; then
-        echo "No 'email' found in '$user_url'." >&2
+        echo "No 'email' found in '$sender_url'." >&2
         echo "$help_message"
         exit 1
     fi
 
-    # Replace the template part of the URL with the username (collaborator).
+    # Replace the template part of the URL with the sender_login (collaborator).
     # https://api.github.com/repos/asbjornu/test/collaborators{/collaborator}
-    collaborator_url="${collaborators_url/\{\/collaborator\}//$username}"
+    collaborator_url="${collaborators_url/\{\/collaborator\}//$sender_login}"
+
+    # Add sender_login and password to the repository clone URL
+    authenticated_repo_url="${repo_clone_url/github\.com/$auth_user:$GITHUB_TOKEN@github.com}"
 }
 
 # echo expanded commands as they are executed (for debugging)
@@ -116,6 +145,7 @@ amend() {
     gh pr comment --body "Amending the last commit."
     git config --global user.name "$name"
     git config --global user.email "$email"
+    git remote set-url origin "$authenticated_repo_url"
     git commit --amend --no-edit
     git push --force
 }
@@ -125,14 +155,14 @@ main() {
     enable_expanded_output
     parse_github_context
 
-    # If the request for </repos/{owner}/{repo}/collaborators/{username}>
+    # If the request for </repos/{owner}/{repo}/collaborators/{sender_login}>
     # fails (404 not found), `gh` should return 1 and thus fail the entire
     # script. For more information, see:
     # https://docs.github.com/en/rest/reference/repos#check-if-a-user-is-a-repository-collaborator
     if gh api -X GET "$collaborator_url" > /dev/null; then
         amend
     else
-        echo "'$username' does not have access to the repository <$repo_url>."
+        echo "'$sender_login' does not have access to the repository <$repo_url>."
     fi
 }
 
