c4d96aff028fe5ea8d0b0a09ccf7b5cd3677fe7e
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-08-20 11:11:35 +0200

    DX-1586: Fix SC2086: Double quote to prevent globbing and word splitting.

--- .github/scripts/apt_update.sh
+++ .github/scripts/apt_update.sh
@@ -2,9 +2,9 @@
 
 JSON=$( cat apt.json )
 
-for PACKAGE in $( echo $JSON | jq -r 'keys | .[]' ); do
+for PACKAGE in $( echo "$JSON" | jq -r 'keys | .[]' ); do
 	VERSION=$( apt-cache policy "$PACKAGE" | grep -oP '(?<=Candidate:\s)(.+)' )
-	JSON=$( echo $JSON | jq '.[$package] = $version' --arg package $PACKAGE --arg version $VERSION )
+	JSON=$( echo "$JSON" | jq '.[$package] = $version' --arg package "$PACKAGE" --arg version "$VERSION" )
 done
 
-echo $JSON | python -m json.tool > apt.json
+echo "$JSON" | python -m json.tool > apt.json
