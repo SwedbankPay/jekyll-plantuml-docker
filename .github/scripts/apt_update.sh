02fc8aabe30887115dfe007cc423d69586627f16
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-08-20 14:19:05 +0200

    DX-1586: Fix whitespace in apt_update.sh

--- .github/scripts/apt_update.sh
+++ .github/scripts/apt_update.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-JSON=$( cat apt.json )
+JSON=$(cat apt.json)
 
-for PACKAGE in $( echo "$JSON" | jq -r 'keys | .[]' ); do
-	VERSION=$( apt-cache policy "$PACKAGE" | grep -oP '(?<=Candidate:\s)(.+)' )
-	JSON=$( echo "$JSON" | jq '.[$package] = $version' --arg package "$PACKAGE" --arg version "$VERSION" )
+for PACKAGE in $(echo "$JSON" | jq -r 'keys | .[]'); do
+    VERSION=$(apt-cache policy "$PACKAGE" | grep -oP '(?<=Candidate:\s)(.+)')
+    JSON=$(echo "$JSON" | jq '.[$package] = $version' --arg package "$PACKAGE" --arg version "$VERSION")
 done
 
 echo "$JSON" | python -m json.tool > apt.json
