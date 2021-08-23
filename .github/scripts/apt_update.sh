168e37baa491f2b5b49b54eb944be7ead9ee98c0
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-08-23 23:39:23 +0200

    More apt update debugging

--- .github/scripts/apt_update.sh
+++ .github/scripts/apt_update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+sudo apt-get update
+
 JSON=$(cat apt.json)
 
 for PACKAGE in $(echo "$JSON" | jq -r 'keys | .[]'); do
     VERSION=$(apt-cache policy "$PACKAGE" | grep -oP '(?<=Candidate:\s)(.+)')
+    echo "Updating '$PACKAGE' to version $VERSION."
     JSON=$(echo "$JSON" | jq '.[$package] = $version' --arg package "$PACKAGE" --arg version "$VERSION")
 done
 
+echo "Writing apt.json to disk:"
+echo "$JSON"
 echo "$JSON" | python -m json.tool > apt.json
